626842577bf5a49c52a1efb63ca67679951e60fd
Jose Marín <jmarinmalagon@gmail.com>
2023-06-02 12:11:42 +0200

    new project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
+
+
+class AuthController extends Controller
+{
+    public function sign_up(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+            'role_id' => 'required'
+        ]);
+
+        // {
+        //     "name":"Jose Marin",
+        //     "email": "josemarin@email.com",
+        //     "password":"123456",
+        //     "password_confirmation":"123456"
+        // }
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'role_id' => $data['role_id'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($res, 201);
+    }
+
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // {
+        //     "email": "josemarin@email.com",
+        //     "password":"123456"
+        // }
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'msg' => 'incorrect username or password'
+            ], 401);
+        }
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($res, 201);
+    }
+
+
+    public function logout(Request $request)
+    {
+         // Get bearer token from the request
+        $accessToken = $request->bearerToken();
+
+        // Get access token from database
+        $token = PersonalAccessToken::findToken($accessToken);
+
+        // Revoke token
+        $token->delete();
+
+        return [
+            'message' => $request->user()
+        ];
+    }
+
+}
